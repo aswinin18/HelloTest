e2559870189312cbf68da91cb0b95d50c11c2950
Ashwini <ashwini@ionosnetworks.com>
2018-10-03 16:12:58 +0530

    maven and testng working

---
+++ src/somepkg/OpenBrowser.java
@@ -0,0 +1,16 @@
+package somepkg;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OpenBrowser {
+
+	@Test
+	public void openB(){
+	System.setProperty("webdriver.chrome.driver", "E:\\jars\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+    
+		driver.get("https://adman.livereachmedia.com/");
+	}
+}
