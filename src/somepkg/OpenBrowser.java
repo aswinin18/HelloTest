c9aa58b5e88a1f35b7930dd0660116e047fd2bd9
Ashwini <ashwini@ionosnetworks.com>
2018-10-10 16:24:08 +0530

    exe

--- src/somepkg/OpenBrowser.java
+++ src/somepkg/OpenBrowser.java
@@ -8,7 +8,7 @@ public class OpenBrowser {
 
 	@Test
 	public void openB(){
-	System.setProperty("webdriver.chrome.driver", "E:\\jars\\chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver", ".\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
     
 		driver.get("https://adman.livereachmedia.com/");
