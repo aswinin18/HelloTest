4581122b9db73323ca6902d3204e389b60945c60
Ashwini <ashwini@ionosnetworks.com>
2018-10-11 14:57:49 +0530

    check url

--- src/somepkg/OpenBrowser.java
+++ src/somepkg/OpenBrowser.java
@@ -1,5 +1,7 @@
 package somepkg;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -7,11 +9,16 @@ import org.testng.annotations.Test;
 public class OpenBrowser {
 
 	@Test
-	public void openB(){
-	//System.setProperty("webdriver.chrome.driver", ".\\chromedriver.exe");
+	public void openB() throws InterruptedException{
+	//System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
 		System.setProperty("webdriver.chrome.driver", "chromedriver");
 		WebDriver driver = new ChromeDriver();
     
 		driver.get("https://adman.livereachmedia.com/");
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		System.out.println("Current URL:"+driver.getCurrentUrl());
+		System.out.println("CLosing browser");
+		driver.quit();
+		
 	}
 }
